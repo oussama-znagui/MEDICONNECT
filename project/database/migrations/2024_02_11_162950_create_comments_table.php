cf2e35e0934aade330973ff34658d5ef9054436e
oussama <znagui.info@gmail.com>
2024-02-11 17:30:03 +0100

    database

--- project/database/migrations/2024_02_11_162950_create_comments_table.php
+++ project/database/migrations/2024_02_11_162950_create_comments_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('favories', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('doctor_id')->constrained('doctors');
         });
     }
 
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('favories');
+        Schema::dropIfExists('comments');
     }
 };
