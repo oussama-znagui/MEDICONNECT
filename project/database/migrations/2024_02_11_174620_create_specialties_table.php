f83ae4ace889e451d031b5772c1a9ac5e79ba37a
oussama <znagui.info@gmail.com>
2024-02-12 09:54:29 +0100

    database

--- project/database/migrations/2024_02_11_174620_create_specialties_table.php
+++ project/database/migrations/2024_02_11_174620_create_specialties_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::disableForeignKeyConstraints();
-        Schema::create('doctors', function (Blueprint $table) {
+        Schema::create('specialties', function (Blueprint $table) {
             $table->id();
-            $table->date('integrationDate');
+            $table->string('specialty');
             $table->timestamps();
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('specialty_id')->constrained('specialties');
         });
     }
 
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('doctors');
+        Schema::dropIfExists('specialties');
     }
 };
--- project/database/migrations/2024_02_11_175011_create_medicins_table.php
+++ project/database/migrations/2024_02_11_175011_create_medicins_table.php
@@ -12,11 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::disableForeignKeyConstraints();
-        Schema::create('doctors', function (Blueprint $table) {
+        Schema::create('medicins', function (Blueprint $table) {
             $table->id();
-            $table->date('integrationDate');
+            $table->string('medicin');
             $table->timestamps();
-            $table->foreignId('user_id')->constrained('users');
             $table->foreignId('specialty_id')->constrained('specialties');
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('doctors');
+        Schema::dropIfExists('medicins');
     }
 };
