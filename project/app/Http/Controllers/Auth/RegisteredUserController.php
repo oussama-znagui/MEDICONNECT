fffbdcbc563846b06b8baa96a32f7fb466235492
oussama <znagui.info@gmail.com>
2024-02-12 14:22:39 +0100

    breeze

--- project/app/Http/Controllers/Auth/RegisteredUserController.php
+++ project/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            // 'sexe' =>$request->
         ]);
 
         event(new Registered($user));
--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'sexe',
+        'birthDate',
+
     ];
 
     /**
@@ -42,4 +45,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function roles()
+    {
+        $this->hasOne(Role::class, 'role_id');
+    }
 }
