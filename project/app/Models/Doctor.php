9b3b7a96933792023b845f2d73cb67dd4c63a0d2
oussama <znagui.info@gmail.com>
2024-02-18 01:24:24 +0100

    comments

--- project/app/Models/Doctor.php
+++ project/app/Models/Doctor.php
@@ -23,4 +23,8 @@ class Doctor extends Model
     {
         return $this->hasMany(Favorie::class);
     }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
