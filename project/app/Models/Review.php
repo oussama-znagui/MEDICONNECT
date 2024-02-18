65773ec56347abfb8e78966a201c7e4523e1c727
oussama <znagui.info@gmail.com>
2024-02-18 16:01:47 +0100

    rating

--- project/app/Models/Review.php
+++ project/app/Models/Review.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 }
